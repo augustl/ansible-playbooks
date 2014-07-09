1ad17b20e1306a9c69227f794fe46feca78cfa93
August Lilleaas <august@augustl.com>
2014-07-10 00:13:42 +0200

    Stealing som OS X stuff from magnars

--- templates/emacs-config-osx.el
+++ templates/emacs-config-osx.el
@@ -1,3 +1,16 @@
 (load-file (concat (file-name-directory (or (buffer-file-name) load-file-name)) "/my.el"))
 
 (custom-set-faces '(default ((t (:height 120 :family "Inconsolata")))))
+
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+;; Norwegian mac-keyboard alt-keys)
+(define-key key-translation-map (kbd "s-8") (kbd "["))
+(define-key key-translation-map (kbd "s-(") (kbd "{"))
+(define-key key-translation-map (kbd "s-9") (kbd "]"))
+(define-key key-translation-map (kbd "s-)") (kbd "}"))
+(define-key key-translation-map (kbd "s-7") (kbd "|"))
+(define-key key-translation-map (kbd "s-/") (kbd "\\"))
+(define-key key-translation-map (kbd "M-s-7") (kbd "M-|"))
