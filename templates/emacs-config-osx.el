87300fda59c9cf573d7dbf3848776e84e912d9c9
August Lilleaas <august@augustl.com>
2014-08-24 19:17:56 +0200

    Nah.

--- templates/emacs-config-osx.el
+++ templates/emacs-config-osx.el
@@ -5,12 +5,3 @@
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
-
-;; Norwegian mac-keyboard alt-keys)
-(define-key key-translation-map (kbd "s-8") (kbd "["))
-(define-key key-translation-map (kbd "s-(") (kbd "{"))
-(define-key key-translation-map (kbd "s-9") (kbd "]"))
-(define-key key-translation-map (kbd "s-)") (kbd "}"))
-(define-key key-translation-map (kbd "s-7") (kbd "|"))
-(define-key key-translation-map (kbd "s-/") (kbd "\\"))
-(define-key key-translation-map (kbd "M-s-7") (kbd "M-|"))
